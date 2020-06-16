3844d6b0f313c0e1556f1dfbef2a620dec3204da
April Webb <aprilewebb@gmail.com>
2020-06-15 22:38:30 -0400

    Updated Alice class

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -8,11 +8,11 @@ public class Alice {
         String quote = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
                 "and of having nothing to do: once or twice she had peeped into the book her sister was reading," +
                 " but it had no pictures or conversations in it, 'and what is the use of a book,' " +
-                "thought Alice 'without pictures or conversation?'";
+                "thought Alice 'without pictures or conversation?'".toLowerCase();
         System.out.println("Please enter a term you'd like to search for: ");
-        String found = input.next();
+        String found = input.next().toLowerCase();
 
-        if (quote.toLowerCase().contains(found.toLowerCase())) {
+        if (quote.contains(found)) {
             System.out.println("Your term was found and it's index is " + quote.indexOf(found) +
                     " and it's length is " + found.length());
             String newQuote = quote.replace(found, "");
