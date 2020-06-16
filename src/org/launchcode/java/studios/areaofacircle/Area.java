4108da0f4281e24fd7ab2cf81872d9aae21f5512
April Webb <aprilewebb@gmail.com>
2020-06-16 10:12:37 -0400

    Created Circle class and updated Area class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -7,7 +7,7 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a radius:");
         double radius = input.nextDouble();
-        double area = (3.14 * radius * radius);
-        System.out.println("The area of a circle of radius " + radius + "is " + area);
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is " + area);
     }
 }
