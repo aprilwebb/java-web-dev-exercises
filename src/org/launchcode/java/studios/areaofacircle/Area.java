0f3dcf7db8bb9c469b2db08fa215933d0c1c28c4
April Webb <aprilewebb@gmail.com>
2020-06-16 21:10:50 -0400

    Added bonus code

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -3,11 +3,28 @@ package org.launchcode.java.studios.areaofacircle;
 import java.util.Scanner;
 
 public class Area {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double radius;
+        double area;
         System.out.println("Enter a radius:");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is " + area);
+        radius = 0;
+        boolean error = false;
+        try {
+            radius = input.nextDouble();
+        } catch (Exception e) {
+            error = true;
+            System.out.println("Radius value is not a number.");
+        }
+
+        input.close();
+        if (error == false) {
+            if (radius < 0) {
+                System.out.println("Radius value is incorrect.");
+            } else {
+                area = Circle.getArea(radius);
+                System.out.println("The area of a circle of radius " + radius + " is " + area);
+            }
+        }
     }
 }
