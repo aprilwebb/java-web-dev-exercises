793862d3670aeff0a75b139cc4cbc7ea5cb36aad
April Webb <aprilewebb@gmail.com>
2020-07-08 20:31:38 -0400

    Adding topping class

--- src/org/launchcode/java/demos/lsn7interfaces/Case.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Case.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class Case {
     private ArrayList<Flavor> flavors = new ArrayList<>();
     private ArrayList<Cone> cones = new ArrayList<>();
+    private ArrayList<Topping> toppings = new ArrayList<>();
 
     public Case(){
         Cone cone1 = new Cone("Waffle", 1.25, new ArrayList<>(Arrays.asList( "gluten")));
@@ -27,6 +28,14 @@ public class Case {
         flavors.add(flavor3);
         flavors.add(flavor4);
         flavors.add(flavor5);
+
+        Topping topping1 = new Topping("Hot fudge", 0.75, new ArrayList<>(Arrays.asList("dairy", "soy")));
+        Topping topping2 = new Topping("Peanuts", 0.50, new ArrayList<>(Arrays.asList("nuts")));
+        Topping topping3 = new Topping("Whipped Cream", 0.50, new ArrayList<>(Arrays.asList("dairy")));
+        toppings.add(topping1);
+        toppings.add(topping2);
+        toppings.add(topping3);
+
     }
 
     public ArrayList<Flavor> getFlavors() {
@@ -43,5 +52,13 @@ public class Case {
 
     public void setCones(ArrayList<Cone> cones) {
         this.cones = cones;
+    }
+
+    public ArrayList<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(ArrayList<Topping> toppings) {
+        this.toppings = toppings;
     }
 }
--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
@@ -31,6 +32,12 @@ public class Main {
         System.out.println("\nCone prices:");
         for (Cone cone : cones){
             System.out.println(cone.getName() + ": $" + cone.getCost());
+        }
+        Comparator toppingComparator = new ToppingComparator();
+        toppings.sort(toppingComparator);
+        System.out.println("\nToppings: ");
+        for(Topping topping : toppings){
+            System.out.println(topping.getName() + ": $" + topping.getCost());
         }
     }
 }
